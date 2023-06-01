6cc099dd6a8cc022b4e9490528b08ceb35b45816
sh0622-kim <66167254+sh0622-kim@users.noreply.github.com>
2023-06-02 00:40:52 +0900

    초기 커밋.

---
+++ SegmentAnythingSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp1;
+using OpenCvSharp;
+
+// sam model path
+string preModelPath = ".\\models\\sam_preprocess.onnx";
+string samModelPath = ".\\models\\sam_vit_h_4b8939.onnx";
+
+// image path
+string inputImagePath = ".\\images\\truck.jpg";
+string resizeImagePath = ".\\results\\resize.jpg";
+string maskImagePath = ".\\results\\mask.jpg";
+string dstImagePath = ".\\results\\dst.jpg";
+
+// point
+Point point = new Point(300, 220);
+
+SamRunner samRunner = new SamRunner(preModelPath, samModelPath);
+
+Mat src = Cv2.ImRead(inputImagePath);
+Mat dst = Mat.Zeros(samRunner.GetInputSize(), MatType.CV_8UC3);
+Cv2.Resize(src, src, samRunner.GetInputSize());
+src.SaveImage(resizeImagePath);
+
+samRunner.LoadImage(src);
+Mat mask = samRunner.GetMask(point);
+mask.SaveImage(maskImagePath);
+
+for (int i = 0; i < src.Rows; i++)
+{
+    for (int j = 0; j < src.Cols; j++)
+    {
+        var bFront = mask.At<byte>(i, j) > 0;
+        float factor = bFront ? 1.0f : 0.2f;
+        dst.Set(i, j, src.At<Vec3b>(i, j) * factor);
+    }
+}
+
+Cv2.DrawMarker(dst, point, Scalar.Lime, MarkerTypes.Cross, 20, 2);
+
+dst.SaveImage(dstImagePath);
---
+++ SegmentAnythingSharp/SamRunner.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using OpenCvSharp;
+
+namespace ConsoleApp1
+{
+    public class SamRunner
+    {
+        private InferenceSession _sessionPreprocess, _sessionSegmentAnything;
+        private int[] _inputShape;
+        private bool _modelLoaded = false;
+        private DenseTensor<float>? _imageEmbeddings;
+
+        public SamRunner(string preprocessModelPath, string segmentAnythingModelPath)
+        {
+            var sessionPrePath = Path.GetFullPath(preprocessModelPath);
+            var sessionSamPath = Path.GetFullPath(segmentAnythingModelPath);
+
+            _sessionPreprocess = new InferenceSession(sessionPrePath);
+
+            _sessionSegmentAnything = new InferenceSession(sessionSamPath);
+
+            _inputShape = _sessionPreprocess.InputMetadata["input"].Dimensions;
+
+            _modelLoaded = true;
+        }
+
+        public Size GetInputSize()
+        {
+            if (!_modelLoaded)
+            {
+                return new Size(0, 0);
+            }
+
+            return new Size(_inputShape[3], _inputShape[2]);
+        }
+
+        public bool LoadImage(Mat image)
+        {
+            var inputTensorValues = new byte[_inputShape[0] * _inputShape[1] * _inputShape[2] * _inputShape[3]];
+
+            for (int i = 0; i < _inputShape[2]; i++)
+            {
+                for (int j = 0; j < _inputShape[3]; j++)
+                {
+                    var pixel = image.Get<Vec3b>(i, j);
+                    inputTensorValues[(i * _inputShape[3]) + j] = pixel[2];
+                    inputTensorValues[(_inputShape[2] * _inputShape[3]) + (i * _inputShape[3]) + j] = pixel[1];
+                    inputTensorValues[(2 * _inputShape[2] * _inputShape[3]) + (i * _inputShape[3]) + j] = pixel[0];
+                }
+            }
+
+            var inputTensor = new DenseTensor<byte>(inputTensorValues, _inputShape.Select(d => d).ToArray());
+
+            string[] inputNamesPre = { "input" };
+
+            var container = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("input", inputTensor)
+            };
+
+            List<DisposableNamedOnnxValue> results = (List<DisposableNamedOnnxValue>)_sessionPreprocess.Run(container);
+
+            _imageEmbeddings = (DenseTensor<float>)results[0].Value;
+
+            return true;
+        }
+
+        public Mat GetMask(Point point)
+        {
+            const int maskInputSize = 256 * 256;
+            float[] inputPointValues = { point.X, point.Y };
+            float[] inputLabelValues = { 1 };
+            float[] maskInputValues = new float[maskInputSize];
+            float[] hasMaskValues = { 0 };
+            float[] origImSizeValues = { _inputShape[2], _inputShape[3] };
+            Array.Clear(maskInputValues, 0, maskInputValues.Length);
+
+            int numPoints = 1;
+            var inputPointShape = new[] { 1, numPoints, 2 };
+            var pointLabelsShape = new[] { 1, numPoints };
+            var maskInputShape = new[] { 1, 1, 256, 256 };
+            var hasMaskInputShape = new[] { 1 };
+            var origImSizeShape = new[] { 2 };
+
+            var pointCoords = new DenseTensor<float>(inputPointValues, inputPointShape);
+            var pointLabels = new DenseTensor<float>(inputLabelValues, pointLabelsShape);
+            var maskInput = new DenseTensor<float>(maskInputValues, maskInputShape);
+            var hasMaskInput = new DenseTensor<float>(hasMaskValues, hasMaskInputShape);
+            var imageSize = new DenseTensor<float>(origImSizeValues, origImSizeShape);
+
+            var container = new List<NamedOnnxValue>
+            {
+                NamedOnnxValue.CreateFromTensor("image_embeddings", _imageEmbeddings),
+                NamedOnnxValue.CreateFromTensor("point_coords", pointCoords),
+                NamedOnnxValue.CreateFromTensor("point_labels", pointLabels),
+                NamedOnnxValue.CreateFromTensor("mask_input", maskInput),
+                NamedOnnxValue.CreateFromTensor("has_mask_input", hasMaskInput),
+                NamedOnnxValue.CreateFromTensor("orig_im_size", imageSize)
+            };
+
+            List<DisposableNamedOnnxValue> results = (List<DisposableNamedOnnxValue>)_sessionSegmentAnything.Run(container);
+
+            var outputMasksValues = results[0].AsTensor<float>().ToArray();
+            var outputMaskSam = new Mat(_inputShape[2], _inputShape[3], MatType.CV_8UC1);
+            for (int i = 0; i < outputMaskSam.Rows; i++)
+            {
+                for (int j = 0; j < outputMaskSam.Cols; j++)
+                {
+                    outputMaskSam.Set(i, j, outputMasksValues[(i * outputMaskSam.Cols) + j] > 0 ? (byte)255 : (byte)0);
+                }
+            }
+            return outputMaskSam;
+        }
+    }
+}
